268b1cb1bb0d155953ae08d65878499357afd3b6
TariqMF <tariqur.rahman@mobilefun.co.uk>
2018-10-22 17:12:20 +0100

    CMD line version

--- Tariq/Controller.java
+++ Tariq/Controller.java
@@ -1,12 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Controller {
 
 	public static void main(String[] args) {
 		
+		String fileName = "numbers";
+		
 		Goldbach goldbach  = new Goldbach();
 		TextUserInterface TUI = new TextUserInterface(goldbach);
+
+		File numberFile = new File(fileName);
 		
-		while(true){
-			TUI.prompt();
-		}
+		TUI.parseFile(numberFile);
 	}
 }
